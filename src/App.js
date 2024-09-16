f791323a740218b86d6e007cc2a9147a828f36a6
Bhanu Prasad Manthri <131452556+mbprasadd@users.noreply.github.com>
2024-09-16 11:25:19 +0000

    completed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Component } from "react";
 import "./styles.css";
 
 class App extends Component {
-  state = {};
+  state = { input: "", mealsData: [] };
 
   componentDidMount() {
     this.displayData();
@@ -12,22 +12,44 @@ class App extends Component {
     let options = {
       method: "GET",
     };
-
-    let url = "www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata";
-
+    let url = "https://www.themealdb.com/api/json/v1/1/search.php?s";
     let response = await fetch(url, options);
     console.log(response);
     if (response.ok) {
       let fetchedData = await response.json();
-      console.log(fetchedData);
+
+      let newData = fetchedData.meals;
+      this.setState({ mealsData: newData });
     }
   };
 
+  searchInput = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  searchButton = () => {};
+
   render() {
+    const { input, mealsData } = this.state;
+
+    let name = mealsData.filter((each) => {
+      each.strMeal == input;
+    });
+
     return (
       <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        <h1>Meal Finder Application</h1>
+        <input
+          type="search"
+          onChange={this.searchInput}
+          id="inputSearch"
+          placeholder="Search Meal"
+        />
+        <br />
+        <button className="buttonsearch" onClick={this.searchButton}>
+          search
+        </button>
+        <p className="mealName">{name}</p>
       </div>
     );
   }
